b34b1c44edffd675a4ac5bdeef3be3dc7d0b2cab
Ponche <142775540+Ponche03@users.noreply.github.com>
2025-02-20 12:00:56 -0600

    pantallas

---
+++ ProcesamientoDeImágenes/ProcesamientoDeImágenes/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProcesamientoDeImágenes
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToVideoPage(object sender, RoutedEventArgs e)
+        {
+            Video newWindow = new Video();
+            newWindow.Show();
+
+            this.Close(); 
+        }
+
+        private void GoToCameraPage(object sender, RoutedEventArgs e)
+        {
+            Camera newWindow = new Camera();
+            newWindow.Show();
+
+            this.Close();
+        }
+
+        private void GoToImageWindow(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+     
+
+    }
+
+}
---
+++ ProcesamientoDeImágenes/ProcesamientoDeImágenes/Video.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProcesamientoDeImágenes
+{
+    /// <summary>
+    /// Interaction logic for Video.xaml
+    /// </summary>
+    public partial class Video : Window
+    {
+        public Video()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToVideoPage(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void GoToCameraPage(object sender, RoutedEventArgs e)
+        {
+
+            Camera newWindow = new Camera();
+            newWindow.Show();
+
+            this.Close();
+        }
+
+        private void GoToImageWindow(object sender, RoutedEventArgs e)
+        {
+
+
+            MainWindow newWindow = new MainWindow();
+            newWindow.Show();
+
+            this.Close();
+
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
